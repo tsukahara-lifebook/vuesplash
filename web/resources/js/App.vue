828c609232fa8764fc8976d1a4ac87f69776d07e
AkitoTsukahara <tsukahara.akito@gmail.com>
2021-01-11 20:46:43 +0900

    login page view

--- web/resources/js/App.vue
+++ web/resources/js/App.vue
@@ -1,9 +1,22 @@
 <template>
     <div>
+        <header>
+            <Navbar />
+        </header>
         <main>
             <div class="container">
                 <RouterView />
             </div>
         </main>
+        <Footer />
     </div>
 </template>
+<script>
+    import Footer from "./components/Footer";
+    import Navbar from "./components/Navbar";
+    export default {
+        components: {
+            Navbar, Footer
+        }
+    }
+</script>
