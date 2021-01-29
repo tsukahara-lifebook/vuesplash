6d96fdaad08f9834b03ce1b31cde128033b06183
AkitoTsukahara <tsukahara.akito@gmail.com>
2021-01-29 22:38:04 +0900

    photo show API test

--- web/routes/api.php
+++ web/routes/api.php
@@ -24,3 +24,4 @@ Route::get('/user', fn() => Auth::user())->name('user');
 Route::post('/photos', 'PhotoController@create')->name('photo.create');
 Route::get('/photos', 'PhotoController@index')->name('photo.index');
 Route::get('/photos/{photo}/download', 'PhotoController@download');
+Route::get('/photos/{id}', 'PhotoController@show')->name('photo.show');
